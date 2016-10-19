38c74162df12e1d77f6c4a866957236040c57815
dragon <rongjihuang@gmail.com>
2016-10-19 19:35:33 +0800

    添加通过 JsonArray 构造 SimpleAdvanceCondition 的构造函数

--- bc-core/src/main/java/cn/bc/core/query/condition/impl/SimpleAdvanceCondition.java
+++ bc-core/src/main/java/cn/bc/core/query/condition/impl/SimpleAdvanceCondition.java
@@ -7,6 +7,7 @@ import cn.bc.core.query.QueryOperator;
 import cn.bc.core.query.condition.AdvanceCondition;
 import cn.bc.core.util.StringUtils;
 
+import javax.json.JsonArray;
 import javax.json.JsonObject;
 
 /**
@@ -19,6 +20,9 @@ public class SimpleAdvanceCondition implements AdvanceCondition {
 	private QueryOperator operator;
 	private Object value;
 
+	public SimpleAdvanceCondition(String id, Object value) {
+		this(id, QueryOperator.Equals, value);
+	}
 
 	public SimpleAdvanceCondition(String id, QueryOperator operator, Object value) {
 		this.id = id;
@@ -38,6 +42,22 @@ public class SimpleAdvanceCondition implements AdvanceCondition {
 		String value = json.getString("value");
 		String type = json.containsKey("type") ? json.getString("type") : null;
 		this.value = StringUtils.convertValueByType(type, value);
+	}
+
+	public SimpleAdvanceCondition(JsonArray item) {
+		// 验证 JsonArray 结构的有效性 [id, value[, type, operator]]
+		if (item == null || item.size() < 2)
+			throw new IllegalArgumentException("error array structure, jsonArray=" + item);
+
+		this.id = item.getString(0);
+
+		// 值
+		String value = item.getString(1);
+		String type = item.size() > 2 ? item.getString(2) : "string";
+		this.value = StringUtils.convertValueByType(type, value);
+
+		// 比较符
+		this.operator = item.size() > 3 ? QueryOperator.symbolOf(item.getString(3)) : QueryOperator.Equals;
 	}
 
 	@Override
